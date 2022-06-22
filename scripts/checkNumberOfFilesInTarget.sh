32e9dc00e18cb648fc266444e6a197212c861025
Andrey Ivanov <v-andivanov@microsoft.com>
2022-06-22 23:22:41 +0400

    v

--- scripts/checkNumberOfFilesInTarget.sh
+++ scripts/checkNumberOfFilesInTarget.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 existingFiles="$( /usr/bin/find target -type f | /usr/bin/sort | xargs )"
+
 if [ "$existingFiles" != "$1" ]; then
-    echo "Existing files: $existingFiles"
-	echo "Expected files: $1"
     exit 1
 fi
