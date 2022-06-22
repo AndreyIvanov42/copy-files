8e8b5dc179b1a1d6ef89b3506825a07b22bc05e1
Andrey Ivanov <v-andivanov@microsoft.com>
2022-06-22 22:06:01 +0400

    remove return

--- scripts/checkNumberOfFilesInTarget.sh
+++ scripts/checkNumberOfFilesInTarget.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # check that all the given files contains in target folder
 existingFiles="$( /usr/bin/find target -type f | /usr/bin/sort | xargs )"
 if [ "$existingFiles" != "$1" ]; 
