b534649556a124845ab56c4e2dab0e832b1c9166
Andrey Ivanov <stayhardordie@gmail.com>
2022-06-22 20:26:40 +0200

    change

--- scripts/checkNumberOfFilesInTarget.sh
+++ scripts/checkNumberOfFilesInTarget.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# check that all the given files contains in target folder
-cd src/copy-files/
-
 existingFiles="$( /usr/bin/find target -type f | /usr/bin/sort | xargs )"
 if [ "$existingFiles" != "$1" ]; 
 then
